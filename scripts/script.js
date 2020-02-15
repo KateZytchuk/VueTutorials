19e62388dcf59b9f210f5ccb50b4e7c217168519
zaets39 <katezytchuk24@gmail.com>
2020-02-15 15:45:27 +0200

    Getting to know about conditions

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,17 @@ var app = new Vue({
       message: 'Hello, Vue!'
     }
   })
-  var app2 = new Vue({
+
+var app2 = new Vue({
     el: '#app-2',
     data: {
       message: 'Вы загрузили эту страницу: ' + new Date().toLocaleString()
+    }
+  })
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: true
     }
   })
